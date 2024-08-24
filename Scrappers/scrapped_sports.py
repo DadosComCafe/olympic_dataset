c18fbb44cbf493c9d8eb755c420bb2d4bd12d5af
DadosComCafe <dadoscomcafe.dev@gmail.com>
2024-08-24 19:01:26 -0300

    (Scrappers/): herda classe base

--- Scrappers/scrapped_sports.py
+++ Scrappers/scrapped_sports.py
@@ -1,34 +1,24 @@
-from bs4 import BeautifulSoup
 from utils import get_request
+from base_scrape import BaseScrape
 
 
-class ScrappedSport:
-    def __init__(self, html_content) -> None:
-        self.html_content = html_content
-        self.soup = self.parse_html()
-        self.my_table = self.soup.find("table", attrs={
-            "class": "table table-striped sortable"}
-        )
-
-
-    def parse_html(self) -> BeautifulSoup:
-        return BeautifulSoup(self.html_content, "html.parser")
+class ScrappedSport(BaseScrape):
     
     def list_content(self) -> list[dict]:
         self.my_list = []
-        for row in self.my_table.find_all("tr"):
+        for row in self.table.find_all("tr"):
             col = row.find_all("td")
             if col:
                 sigla = str(col[0]).replace("<td>", "").replace("</td>", "")
                 esporte = str(col[1]).replace(f"""<td><a href="/sports/{sigla}">""", "").replace("</a></td>", "")
                 estacao = str(col[3]).replace("<td>", "").replace("</td>", "")
-                print(f"Sigla: {sigla}", f"Esporte: {esporte}", f"Estação: {estacao}")
                 self.my_list.append({"sigla": sigla, "esporte": esporte, "estação": estacao})
+        return self.my_list
 
 
 if __name__ == "__main__":
-    import ipdb
-    ipdb.set_trace()
+    #import ipdb
+    #ipdb.set_trace()
     site = "https://www.olympedia.org/sports"
     html_content = get_request(site)
     scrapped_sport_obj = ScrappedSport(html_content=html_content)
